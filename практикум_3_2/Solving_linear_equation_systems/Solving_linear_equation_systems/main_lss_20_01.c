a440071f2745ed93666df2ccef367a6253d90092
Yaromir Kobikov <yaromir.kobikov@finnetlimited.com>
2019-02-28 21:54:52 +0500

    some_wrong

--- практикум_3_2/Solving_linear_equation_systems/Solving_linear_equation_systems/main_lss_20_01.c
+++ практикум_3_2/Solving_linear_equation_systems/Solving_linear_equation_systems/main_lss_20_01.c
@@ -13,19 +13,25 @@ int main() {
  double *B;
 
  double *A_tmp;
+ double **tem_vec;
 
  file = fopen("test.txt", "r");
  fscanf(file, "%d", &a);
 
  A = (double**)malloc(a * sizeof(double*));
+ tem_vec = (double**)malloc(a * sizeof(double*));
+
  A_tmp = (double*)malloc(a * sizeof(double));
 
 
 
  for (int i = 0; i < a; i++) {
 	A[i] = (double*)malloc(a * sizeof(double));
+	tem_vec[i] = (double*)malloc(a * sizeof(double));
+
 	for (int j = 0; j < a; j++) {
 	 fscanf(file, "%lf", &A[i][j]);
+	 tem_vec[i][j] = A[i][j];
 	}
  }
 
@@ -53,10 +59,9 @@ int main() {
 	double now_el = A[i][i];
 
 	for (int j = i; j < a; j++) {
-
 	 A[i][j] = A[i][j] / now_el;
-	}
 
+	}
 	for (int j = i + 1; j < a; j++) {
 	 B[j] = B[j] - B[i] * A[j][i];
 	 now_el = A[j][i];
@@ -75,30 +80,40 @@ int main() {
 	}
 	printf(" %lf\n", B[i]);
  }printf("\n");
+ printf("///////////////////////////////////////////////////\n");
 
 
 
- a = a - 1;
+ 
  double some_tmp;
 
- for (int i = a; i != 0; i--) {
+ for (int i = a - 1; i > -1; i--) {
 	some_tmp = B[i];
 
-	for (int j = i ; j != a - i; j--) {
-	 printf("%d ", j);
-	 some_tmp = some_tmp - A[i][j] * B[i];
+	for (int j = a - 1 ; j > i; j--) {
+	 some_tmp = some_tmp - A[i][j] * B[j];
 
-	} printf("\n");
+	}
 	A_tmp[i] = some_tmp;
  }
 
 
 
-
- a = a + 1;
  for (int i = 0; i < a; i++) {
 	printf("x_%d = %lf\n", i + 1, A_tmp[i]);
  }printf("\n");
+
+
+
+ 
+ for (int i = 0; i < a; i++) {
+	double check = 0;
+	for (int j = 0; j < a; j++) {
+	 check += A_tmp[j] * tem_vec[i][j];
+	}
+	printf("%lf\n", check);
+	check = 0;
+ }
 
  system("pause");
  return 0;
