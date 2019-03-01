04c1279d2be27955b533a5072e7fbaa8f62462bd
Yaromir Kobikov <yaromir.kobikov@finnetlimited.com>
2019-03-01 16:30:24 +0500

    work_but_change_matrix_system

--- практикум_3_2/Solving_linear_equation_systems/Solving_linear_equation_systems/main_lss_20_01.c
+++ практикум_3_2/Solving_linear_equation_systems/Solving_linear_equation_systems/main_lss_20_01.c
@@ -12,39 +12,36 @@ int main() {
  double **A;
  double *B;
 
- double *answer;
- double **A_true;
- double *index_vector;
+ double *X;
+ double *tmp;
 
  file = fopen("test.txt", "r");
  fscanf(file, "%d", &a);
 
  A = (double**)malloc(a * sizeof(double*));
- A_true = (double**)malloc(a * sizeof(double*));
- answer = (double*)malloc(a * sizeof(double));
- index_vector = (double*)malloc(a * sizeof(double));
+ X = (double*)malloc(a * sizeof(double));
+ tmp = (double*)malloc(a * sizeof(double));
+
  for (int i = 0; i < a; i++) {
-	index_vector[i] = i;
+	tmp[i] = i;
  }
+
+ double **A_true;
+ A_true = (double**)malloc(a * sizeof(double*));
 
 
  for (int i = 0; i < a; i++) {
 	A[i] = (double*)malloc(a * sizeof(double));
 	A_true[i] = (double*)malloc(a * sizeof(double));
-
 	for (int j = 0; j < a; j++) {
 	 fscanf(file, "%lf", &A[i][j]);
 	 A_true[i][j] = A[i][j];
 	}
  }
-
  B = (double*)malloc(a * sizeof(double));
- 
  for (int i = 0; i < a; i++) {
 	 fscanf(file, "%lf", &B[i]);
- }
-
- printf("%d\n", a);
+ } printf("%d\n", a);
 
 
  for (int i = 0; i < a; i++) {
@@ -55,9 +52,101 @@ int main() {
  }printf("\n");
 
 
+ ////////////////////////////////////////////////////////////////////////////////////
+
+
+ for (int i = 0; i < a; i++) {
+
+	double max = A[i][i];
+	int column = 0;
+	int blabla = 0;
+	for (int j = i; j < a; j++) {
+	 if (abs(max) < abs(A[i][j])) {
+		column = j;
+		max = A[i][j];
+		blabla = 1;
+	 }
+	}
+	if (blabla == 1) {
+	 double change_column;
+	 change_column = tmp[i];
+	 tmp[i] = tmp[column];
+	 tmp[column] = change_column;
+
+	 for (int j = 0; j < a; j++) {
+		change_column = A[j][i];
+		A[j][i] = A[j][column];
+		A[j][column] = change_column;
+	 }
+	 blabla = 0;
+	}
+	
+	double save_1 = A[i][i];
+	B[i] = B[i] / A[i][i];
+
+	for (int j = 0; j < a; j++) {
+	 A[i][j] = A[i][j] / save_1;
+	}
+
+	for (int j = i + 1; j < a; j++) {
+	 save_1 = A[j][j - 1];
+	 B[j] = B[j] - B[i] * A[j][j - 1];
+
+	 for (int k = i; k < a; k++) {
+		A[j][k] = A[j][k] - save_1 * A[i][k];
+	 }
+	}
+ }
+
+ 	for (int i = 0; i < a; i++) {
+	 for (int j = 0; j < a; j++) {
+		printf("%lf ", A[i][j]);
+	 }
+	 printf(" %lf\n", B[i]);
+	}printf("\n");
+
+	double some_tmp;
+
+	for (int i = a - 1; i > -1; i--) {
+	 some_tmp = B[i];
+
+	 for (int j = a - 1; j > i; j--) {
+		int abc = 0;
+		for (int k = 0; k < a; k++) {
+		 if (tmp[k] == j) {
+			abc = tmp[k];
+			break;
+		 }
+		}
+		some_tmp -= A[i][j] * X[abc];
+	 }
+	 X[i] = some_tmp;
+	}
+
+	for (int i = 0; i < a; i++)
+	{
+	 printf("%lf ", X[i]);
+	}printf("\n");
+
+	for (int i = 0; i < a; i++) {
+	 printf("x_%d = %lf\n", i + 1, X[i]);
+	}printf("\n");
+
+
+	//for (int i = 0; i < a; i++) {
+	// double abcd = 0;
+	// for (int j = 0; j < a; j++) {
+	//	abcd += A_true[i][j] * X[j];
+	// }
+	// printf("%lf\n", abcd);
+	//}
+
+
+
+
 
  ////////////////////////////////////////////////////////////////////////////////////
- for (int i = 0; i < a; i++) {
+ /*for (int i = 0; i < a; i++) {
 	double max = A[i][i];
 	int change = 0;
 	int column;
@@ -66,28 +155,31 @@ int main() {
 		max = A[i][j];
 		column = j;
 		change = 1;
+		printf("%d\n", j);
 	 }
 	}
-	
 
 	if (change == 1) {
+
+	 for (int q = 0; q < a; q++) {
+	 } printf("\n");
+		printf("%lf ", tmp[q]);
+	 printf("change\n");
 	 double tr;
-	 index_vector[i] = column;
-	 index_vector[column] = i;
+	 tmp[i] = column;
+	 tmp[column] = i;
 	 for (int k = i; k < a; k++) {
-		tr = A[i][k];
+		tr = A[k][i];
 		A[k][i] = A[k][column];
 		A[k][column] = tr;
-		for (int i = 0; i < a; i++) {
-		 for (int j = 0; j < a; j++) {
-			printf("%lf ", A[i][j]);
-		 }
-		 printf(" %lf\n", B[i]);
-		}printf("\n");
 	 }
 	 change = 0;
+	 for (int q = 0; q < a; q++) {
+		printf("%lf ", tmp[q]);
+	 } printf("\n");
+	 printf("use\n");
 	}
-	printf("use");
+
 	for (int i = 0; i < a; i++) {
 	 for (int j = 0; j < a; j++) {
 		printf("%lf ", A[i][j]);
@@ -107,7 +199,7 @@ int main() {
 	 now_el = A[j][i];
 
 	 for (int k = i; k < a; k++) {
-
+		printf("\n%lf %lf %lf %lf\n", A[j][k], " = ", A[j][k], " - ", now_el, " * ", A[i][k]);
 		A[j][k] = A[j][k] - now_el * A[i][k];
 	 }
 	}
@@ -139,18 +231,18 @@ int main() {
 	for (int j = a - 1 ; j > i; j--) {
 	 for (int w = 0; w < a; w++)
 	 {
-		if (index_vector[w] == j) {
+		if (tmp[w] == j) {
 		 some_tmp = some_tmp - A[i][w] * B[j];
 		}
 	 }
 	}
-	answer[i] = some_tmp;
+	X[i] = some_tmp;
  }
 
 
 
  for (int i = 0; i < a; i++) {
-	printf("x_%d = %lf\n", i + 1, answer[i]);
+	printf("x_%d = %lf\n", i + 1, X[i]);
  }printf("\n");
 
 
@@ -159,11 +251,11 @@ int main() {
  for (int i = 0; i < a; i++) {
 	double check = 0;
 	for (int j = 0; j < a; j++) {
-	 check += answer[j] * A_true[i][j];
+	 check += X[j] * A_true[i][j];
 	}
 	printf("%lf\n", check);
 	check = 0;
- }
+ }*/
 
  system("pause");
  return 0;
