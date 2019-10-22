206fbab28f0541c55e5125ee2e4220b6a225865e
YaromirKo <34893823+YaromirKo@users.noreply.github.com>
2019-10-22 20:04:47 +0500

    comments were added to the evс module, as well as a small refactoring of variable names

--- praktikum_4_1/evc_11_01.c
+++ praktikum_4_1/evc_11_01.c
@@ -18,10 +18,11 @@ void print_m2(int n, double * A) {
 
 int evc_11_01(int n, int max_iterations, double epsilon, double * A, double * E, double * tmp, double precision) {
 
-    int endless_computing = 1;
-    if (max_iterations > 0) endless_computing = max_iterations;
+    // если значение max_iterations <= 0, то алгоритм должен выполнятся до получения требуемой точности (без ограничения на число итераций).
+    int steps_count = 1;
+    if (max_iterations > 0) steps_count = max_iterations;
 
-    for (int k = 0; k < endless_computing; ++k) {
+    for (int k = 0; k < steps_count; ++k) {
 
         // отладочная печать
         if (var_for_debug == 1) {
@@ -59,32 +60,35 @@ int evc_11_01(int n, int max_iterations, double epsilon, double * A, double * E,
             for (int j = 0; j < n; ++j) {
                 _A(A, n, i, j) = 0;
                 for (int l = 0; l < n; ++l) {
-
+                    // определяем элемент матрицы L
                     if (l == j) elem_L = 1;
                     else if (l - 1 == j) elem_L = L(tmp, n, l, j);
                     else elem_L = 0;
-
+                    // определяем элемент матрицы R
                     if (i > l) elem_R = 0;
                     else elem_R = R(tmp, n, i, l);
 
                     _A(A, n, i, j) += elem_L * elem_R;
+                    // если элемент матрицы <= precision, тогда на место этого элемента ставим 0
                     if (fabs(_A(A, n, i, j)) <= precision) _A(A, n, i, j) = 0;
                 }
             }
         }
 
-        int count = 0;
+        int diagonal_el_counter = 0; // счетчик диагональных элементов
         int _switch = 1;
 
         if (k % 2 == 0) _switch = 0;
 
+        // сравниваем диагональные элементы матрицы A_k и A_k+1
         for (int i = 0; i < n; ++i) {
             tmp(tmp, n, (n + _switch), i) = _A(A, n, i, i);
             if (k > 0) {
-                if (_switch == 0) if (fabs(tmp(tmp, n, n, i) - tmp(tmp, n, (n + 1), i)) < epsilon) count++;
-                else if (fabs(tmp(tmp, n, (n + 1), i) - tmp(tmp, n, n, i)) < epsilon) count++;
-                E(E, n, 0, i) = _A(A, n, i, i);
+                if (_switch == 0) if (fabs(tmp(tmp, n, n, i) - tmp(tmp, n, (n + 1), i)) < epsilon) diagonal_el_counter++;
+                else if (fabs(tmp(tmp, n, (n + 1), i) - tmp(tmp, n, n, i)) < epsilon) diagonal_el_counter++;
             }
+            // заполняем массив E диагональными элементами матрицы R*L  на k шаге
+            E(E, n, 0, i) = _A(A, n, i, i);
         }
 
         // отладочная печать - печатаем матрицу A, полученную после перемножения R и L
@@ -93,12 +97,12 @@ int evc_11_01(int n, int max_iterations, double epsilon, double * A, double * E,
             print_m2(n, A);
         }
 
-        if (count == n) {
+        if (diagonal_el_counter == n) {
             // 0 - работа завершена успешно
             printf("the difference of the diagonal elements k + 1 and k matrices is less than epsilon\n");
             return 0;
         }
-        endless_computing++;
+        steps_count++; // тк max_iterations <= 0 и не достигнута необходимая точность, то увеличиваем счетчик шагов
     }
 
     // 1 - метод не сходится за указанное число итераций
