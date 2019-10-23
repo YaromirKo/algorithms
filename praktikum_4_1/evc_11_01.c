13bfaefbe1b0348ba23d5cbfaa888110b107a884
YaromirKo <34893823+YaromirKo@users.noreply.github.com>
2019-10-23 20:26:33 +0500

    code upgrade

--- praktikum_4_1/evc_11_01.c
+++ praktikum_4_1/evc_11_01.c
@@ -1,31 +1,27 @@
 // вычислительный модуль
 #include "task_11_01.h"
 
-#define _A(matrix, len, row, col) (matrix + len * row)[col]
-#define L(matrix, len, row, col) (matrix + len * row)[col]
-#define R(matrix, len, row, col) (matrix + len * row)[col]
-#define tmp(matrix, len, row, col) (matrix + len * row)[col]
-#define E(matrix, len, row, col) (matrix + len * row)[col]
+#define ELEM(matrix, len, row, col) (matrix + len * row)[col]
 
 // печать матрицы n * n
 void print_m2(int n, double * A) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            printf("%*lf", 13, _A(A, n, i, j));
+            printf("%*lf", 13, ELEM(A, n, i, j));
         } printf("\n");
     } printf("\n");
 }
 
-void bubble_sort(int n, double * matrix) {
+void bubble_sort(int n, double * vector) {
 
     int check = 0;
     double swap_el;
     for (int i = 0; i < n - 1; ++i) {
         for (int j = 0; j < n - 1 - i; ++j) {
-            if (matrix[j] > matrix[j + 1]) {
-                swap_el = matrix[j];
-                matrix[j] = matrix[j + 1];
-                matrix[j + 1] = swap_el;
+            if (vector[j] > vector[j + 1]) {
+                swap_el = vector[j];
+                vector[j] = vector[j + 1];
+                vector[j + 1] = swap_el;
                 check = 1;
             }
         }
@@ -50,16 +46,16 @@ int evc_11_01(int n, int max_iterations, double epsilon, double * A, double * E,
         }
 
         // заполняем первую строку матрицы R
-        for (int i = 0; i < n; ++i) R(tmp, n, 0, i) = _A(A, n, 0, i);
+        for (int i = 0; i < n; ++i) ELEM(tmp, n, 0, i) = ELEM(A, n, 0, i);
 
         // заполняем массив tmp - служащиц для хранеия необходимых элементов матриц L и R для последующего их перемножения
         for (int i = 1; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (i - 1 == j) { // элементы 2ой главной диагонали для L
-                    L(tmp, n, i, (i - 1)) = _A(A, n, i, (i - 1)) / R(tmp, n, (i - 1), (i - 1));
+                    ELEM(tmp, n, i, (i - 1)) = ELEM(A, n, i, (i - 1)) / ELEM(tmp, n, (i - 1), (i - 1));
                 }
                 if (i == j || j > i) { // все элементы выше и включая саму диагональ матрицы R
-                    R(tmp, n, i, j) = _A(A, n, i, j) - L(tmp, n, i, (i - 1)) * R(tmp, n, (i - 1), j);
+                    ELEM(tmp, n, i, j) = ELEM(A, n, i, j) - ELEM(tmp, n, i, (i - 1)) * ELEM(tmp, n, (i - 1), j);
                 }
             }
         }
@@ -76,19 +72,19 @@ int evc_11_01(int n, int max_iterations, double epsilon, double * A, double * E,
         // перемножаем R and L
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
-                _A(A, n, i, j) = 0;
+                ELEM(A, n, i, j) = 0;
                 for (int l = 0; l < n; ++l) {
                     // определяем элемент матрицы L
                     if (l == j) elem_L = 1;
-                    else if (l - 1 == j) elem_L = L(tmp, n, l, j);
+                    else if (l - 1 == j) elem_L = ELEM(tmp, n, l, j);
                     else elem_L = 0;
                     // определяем элемент матрицы R
                     if (i > l) elem_R = 0;
-                    else elem_R = R(tmp, n, i, l);
+                    else elem_R = ELEM(tmp, n, i, l);
 
-                    _A(A, n, i, j) += elem_L * elem_R;
+                    ELEM(A, n, i, j) += elem_L * elem_R;
                     // если элемент матрицы <= precision, тогда на место этого элемента ставим 0
-                    if (fabs(_A(A, n, i, j)) <= precision) _A(A, n, i, j) = 0;
+                    if (fabs(ELEM(A, n, i, j)) <= precision) ELEM(A, n, i, j) = 0;
                 }
             }
         }
@@ -100,13 +96,13 @@ int evc_11_01(int n, int max_iterations, double epsilon, double * A, double * E,
 
         // сравниваем диагональные элементы матрицы A_k и A_k+1
         for (int i = 0; i < n; ++i) {
-            tmp(tmp, n, (n + _switch), i) = _A(A, n, i, i);
+            ELEM(tmp, n, (n + _switch), i) = ELEM(A, n, i, i);
             if (k > 0) {
-                if (_switch == 0) if (fabs(tmp(tmp, n, n, i) - tmp(tmp, n, (n + 1), i)) < epsilon) diagonal_el_counter++;
-                else if (fabs(tmp(tmp, n, (n + 1), i) - tmp(tmp, n, n, i)) < epsilon) diagonal_el_counter++;
+                if (_switch == 0) if (fabs(ELEM(tmp, n, n, i) - ELEM(tmp, n, (n + 1), i)) < epsilon) diagonal_el_counter++;
+                else if (fabs(ELEM(tmp, n, (n + 1), i) - ELEM(tmp, n, n, i)) < epsilon) diagonal_el_counter++;
             }
             // заполняем массив E диагональными элементами матрицы R*L  на k шаге
-            E(E, n, 0, i) = _A(A, n, i, i);
+            ELEM(E, n, 0, i) = ELEM(A, n, i, i);
         }
 
         // отладочная печать - печатаем матрицу A, полученную после перемножения R и L
